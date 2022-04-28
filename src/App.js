5e49d1587208b3a4f922f603825f11f1d5e64ee4
jsubak <tnwjd100@gmail.com>
2022-04-28 16:20:04 +0900

    분리 및 카드꾸미기

--- src/App.js
+++ src/App.js
@@ -4,14 +4,9 @@ import { ThemeProvider, createTheme } from '@mui/material/styles';
 import Container from '@mui/material/Container';
 import Switch from '@mui/material/Switch';
 import Box from '@mui/material/Box';
-import axios from 'axios';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
 import Grid from '@mui/material/Grid';
 
-import { cityLatLon } from './dataset/WeatherData';
+
 import WeatherCard from './components/WeatherCard';
 import UserCardList from './components/UserCardList';
 import { makeUserDatas } from './Utils';
@@ -22,38 +17,12 @@ const userDatas = makeUserDatas(5000);
 
 function App() {
   const [useDarkMode, setUseDarkMode] = useState(true);
-  const [weatherData, setWeatherData] = useState(null);
-  const [apiError, setApiError] = useState(null);
-  const [selectedCityData, setSelectedCityData] = useState({name: "안양", lat: 37.39692, lon: 126.91425});
-
+  
   const handleChange = (event) => {
     console.log(event);
     setUseDarkMode(event.target.checked);
     // setUseDarkMode(useDarkMode ? false : true);
   }
-
-  const selectHandleChange = (event) => {
-    /* const cityName = (event.target.value); const findCityLatLon = cityLatLon.find(element => element.name === cityName);*/
-    const cityName = cityLatLon.find(data => data.name === event.target.value);
-    setSelectedCityData(cityName);
-  }
-
-/*  useEffect(() => {
-    
-  }, []); */
-
-  useEffect(() => {
-    const callApi = async() => {
-      try {
-        const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${selectedCityData.lat}&lon=${selectedCityData.lon}&lang=kr&units=metric&appid=f2e4e12af2997b3bacce4b18f9aedd05`)
-        setWeatherData(result.data);
-      }catch(err){
-        setApiError(err);
-      }
-    }
-    callApi()
-    console.log("commponent did mount");
-  }, [selectedCityData])
 
   useEffect(() => {
     console.log(`theme 변경됨 -> ${useDarkMode}`);
@@ -75,24 +44,8 @@ function App() {
         p: 1,
       }}>
         <Container maxWidth="lg" sx={{p:1}}>
-          <FormControl>
-            <InputLabel id="selected-city-label">도시</InputLabel>
-            <Select
-              labelId="selected-city-label"
-              id="selected-city"
-              value={selectedCityData.name}
-              label="도시"
-              onChange={selectHandleChange}
-            >
-              {cityLatLon.map((city)=> 
-                <MenuItem value={city.name}>{city.name}</MenuItem>
-                )}
-            </Select>
-          </FormControl>
           <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs:4, sm: 8, md: 12 }}>
-            <WeatherCard weatherData={weatherData} apiError={apiError} />
-            <WeatherCard weatherData={weatherData} apiError={apiError} />
-            <WeatherCard weatherData={weatherData} apiError={apiError} />
+            {[1,2,3,4,5,6,7,8,9].map((no) =>{return <WeatherCard id={no} />})}
           </Grid>
 
           <Switch
--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,9 +1,54 @@
+import React, { useState, useEffect } from "react";
 import { Typography } from "@mui/material";
 import Grid from '@mui/material/Grid';
-import { weather_mapping_data } from "../dataset/WeatherData"
+import axios from 'axios';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { weather_mapping_data, cityLatLon } from "../dataset/WeatherData"
 
 function WeatherCard(props) {
-    const {weatherData, apiError} = props;
+    const { id } = props;
+    const defaultCityName = localStorage.getItem(id+'_city') || "안양"
+    const [weatherData, setWeatherData] = useState(null);
+    const [apiError, setApiError] = useState(null);
+    // cityLatLon.find(data=> data.name === props.cityName));
+
+    const findCity = cityLatLon.find(data=> data.name === defaultCityName)
+    const [selectedCityData, setSelectedCityData] = useState(findCity)
+
+    const selectHandleChange = (event) => {
+        /* const cityName = (event.target.value); const findCityLatLon = cityLatLon.find(element => element.name === cityName);*/
+        const cityName = cityLatLon.find(data => data.name === event.target.value);
+        setSelectedCityData(cityName);
+        localStorage.setItem(id+'_city', cityName.name)
+    }
+
+    useEffect(() => {
+        const callApi = async() => {
+          // 현재시간 - 로컬스토리에 저장한 시간 = 로컬스토리지에 저장한 시간으로부터 흘러간 시간이 나옴
+          // 흘러간 시간이 10분 미만이면 로컬스토리지에 저장한 날씨데이터를 활용
+          // 흘러간 시간이 10분 이상이면 openAPI 호출
+          const cityName = selectedCityData.name
+          const cityGetDate = cityName+'_저장시간'
+          if((Date.now() - localStorage.getItem(cityGetDate)) /1000 / 60 > 60) {
+            try {
+                const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${selectedCityData.lat}&lon=${selectedCityData.lon}&lang=kr&units=metric&appid=f2e4e12af2997b3bacce4b18f9aedd05`)
+                setWeatherData(result.data);
+                localStorage.setItem(cityName, JSON.stringify(result.data))
+                localStorage.setItem(cityGetDate, Date.now()) // 현재 시간
+            }catch(err){
+              setApiError(err);
+            }
+          }else {
+            setWeatherData(JSON.parse(localStorage.getItem(cityName)));
+          }
+        }
+        callApi()
+        console.log("commponent did mount");
+    }, [selectedCityData])
+    
     const markeWeatherInfo = () => {
         const {temp, temp_min, temp_max, feels_like, humidity} = weatherData.main;
         const {main, icon} = weatherData.weather[0];
@@ -11,11 +56,26 @@ function WeatherCard(props) {
         const parseWeatherData = weather_mapping_data[main] ? weather_mapping_data[main] : weather_mapping_data["Etc"];
 
         return <Grid item xs={1} sm={2} md={4}>
-            <Typography>{`현재날씨: ${parseWeatherData.name}`}</Typography>
+            <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+                <InputLabel id="selected-city-label">도시</InputLabel>
+                <Select
+                labelId="selected-city-label"
+                id="selected-city"
+                value={selectedCityData.name}
+                label="도시"
+                onChange={selectHandleChange}
+                >
+                {cityLatLon.map((city)=> 
+                    <MenuItem value={city.name}>{city.name}</MenuItem>
+                    )}
+                </Select>
+            </FormControl>
+            <Typography variant="h6" gutterBottom component="div">{`현재날씨: ${parseWeatherData.name}`}</Typography>
             <parseWeatherData.icon sx={{fontSize: 100, color: 'red'}} />
             <img src={iconUrl} alt="현재날씨 아이콘" />
-            <Typography>{`현재온도: ${temp}℃ 체감온도: ${feels_like}℃`}</Typography>
-            <Typography>{`최저기온: ${temp_min}℃ 최고기온: ${temp_max}℃ 습도: ${humidity}%`}</Typography>
+            <Typography variant="subtitle1" gutterBottom component="div">{`현재온도: ${temp}℃ 체감온도: ${feels_like}℃`}</Typography>
+            <Typography variant="subtitle1" gutterBottom component="div">{`최저기온: ${temp_min}℃ 최고기온: ${temp_max}℃`}</Typography>
+            <Typography>{`습도: ${humidity}%`}</Typography>
         </Grid>
     }
     
